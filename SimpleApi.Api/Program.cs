0ccdfc24c64b2764969618e2aa476387d25e7a42
Hendrick <hsumeck@gmail.com>
2022-07-18 22:40:45 -0300

    Swagger documentation

--- SimpleApi.Api/Program.cs
+++ SimpleApi.Api/Program.cs
@@ -4,10 +4,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddInfraDataConfiguration();
 
 var app = builder.Build();
+
 app.UseSwagger();
 app.UseSwaggerUI();
+app.UseHttpsRedirection();
+app.MapControllers();
 
 app.Run();
